788280881910eba58b7c1017dc2439e737fe4628
prusswan <prusswan@gmail.com>
2016-08-05 18:23:19 +0800

    completed wk5 assignment

--- wk5_assignment/js/ajax-utils.js
+++ wk5_assignment/js/ajax-utils.js
@@ -8,27 +8,27 @@ var ajaxUtils = {};
 function getRequestObject() {
   if (window.XMLHttpRequest) {
     return (new XMLHttpRequest());
-  } 
+  }
   else if (window.ActiveXObject) {
     // For very old IE browsers (optional)
     return (new ActiveXObject("Microsoft.XMLHTTP"));
-  } 
+  }
   else {
     global.alert("Ajax is not supported!");
-    return(null); 
+    return(null);
   }
 }
 
 
 // Makes an Ajax GET request to 'requestUrl'
-ajaxUtils.sendGetRequest = 
+ajaxUtils.sendGetRequest =
   function(requestUrl, responseHandler, isJsonResponse) {
     var request = getRequestObject();
-    request.onreadystatechange = 
-      function() { 
-        handleResponse(request, 
+    request.onreadystatechange =
+      function() {
+        handleResponse(request,
                        responseHandler,
-                       isJsonResponse); 
+                       isJsonResponse);
       };
     request.open("GET", requestUrl, true);
     request.send(null); // for POST only
@@ -42,7 +42,7 @@ function handleResponse(request,
                         responseHandler,
                         isJsonResponse) {
   if ((request.readyState == 4) &&
-     (request.status == 200)) {
+     (request.status == 200 || request.status == 0)) {
 
     // Default to isJsonResponse = true
     if (isJsonResponse == undefined) {
